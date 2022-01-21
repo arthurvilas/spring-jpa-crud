99a3f0fb799f37fb3063c5f5f33001b654ba95e0
Arthur Vilas B. Santos <arthur_santos@virgo.atlantico.net.br>
2022-01-20 21:50:08 -0300

    Added Product entity with many to many association

--- src/main/java/com/example/demo/entities/Category.java
+++ src/main/java/com/example/demo/entities/Category.java
@@ -1,9 +1,13 @@
 package com.example.demo.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Table
 @Entity(name = "tb_category")
@@ -14,6 +18,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incremented primary key
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -40,6 +48,10 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,4 +64,5 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
